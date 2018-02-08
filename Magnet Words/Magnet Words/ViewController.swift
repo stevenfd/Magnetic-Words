84c173d29b44934ea45dac797fbf0fc784a14551
Steven Domitrz <sfd7250@rit.edu>
2018-02-08 15:41:49 -0500

    Starting to get word placement working, creation of labels and picking of words done. Placement next

--- Magnet Words/Magnet Words/ViewController.swift
+++ Magnet Words/Magnet Words/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
+let WORDS_ON_SCREEN = 20
+
+//Buffer on the side of the application
+let SIDE_BUFFER = 16
+//Buffer between words and rows
+let WORD_BUFFER = 8
+
+//TODO: Load this in through a file?
+let words = ["could","cloud","bot","bit","ask","a","geek","flame","file","ed","create","like","lap","is","ing","I","her","drive","get","soft","screen","protect","online","meme","to","they","that","tech","space","source","y","write","while"]
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.blue
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Function to place new words on the screen
+    func placeNewWords() {
+        var row = 0
+        var furthestLabel = 0
+        
+        for index in 1...WORDS_ON_SCREEN {
+            let word = UILabel()
+            word.backgroundColor = UIColor.white
+            //Get a random word from the list of words
+            //TODO: Prevent duplicates? Maybe not as big a deal with lots of words?
+            let randNum = Int(arc4random_uniform(UInt32(words.count)))
+            word.text = words[randNum]
+            word.sizeToFit()
+            
+            //Now lets do the work of placing these in rows
+            
+        }
     }
-
-
 }
 
