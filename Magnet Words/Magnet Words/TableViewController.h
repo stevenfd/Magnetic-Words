44c0609f4cd4a032e44c00c858d01a7cd4a75d19
Steven <sfd7250@rit.edu>
2018-02-16 13:14:54 -0500

    Added table viewcontroller, implemented dummy data rows

---
+++ Magnet Words/Magnet Words/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Book Word Magnets
+//
+//  Created by Student on 2/16/18.
+//  Copyright © 2018 Steven Domitrz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
